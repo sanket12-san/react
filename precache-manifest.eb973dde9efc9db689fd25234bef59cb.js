54e4b908406de47e6e714c555bd928c6cb065054
Sanket Thareja <sanketthareja123@gmail.com>
2020-07-27 14:56:44 +0530

    Updates

---
+++ precache-manifest.eb973dde9efc9db689fd25234bef59cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d12f02aedeee8782c952c761aafc7f1",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6b34375b3e0e601c7b93",
+    "url": "/react/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "d74d3104b814c2e2ec02",
+    "url": "/react/static/js/2.b8fbbb19.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react/static/js/2.b8fbbb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b34375b3e0e601c7b93",
+    "url": "/react/static/js/main.8d6814b7.chunk.js"
+  },
+  {
+    "revision": "a928c4dceb711baf9e4c",
+    "url": "/react/static/js/runtime-main.0897dc1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
